ace33c4a403919ce1d2fef649f3b20891822b271
kostchei <56498970+kostchei@users.noreply.github.com>
2023-05-14 22:51:24 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import skills from './data/skills.json';
+import resources from './data/resources.json';
 
-function App() {
+// Define skill list and resources
+const skills = [
+  { skill: "Skill 1", stat: "Stat 1" },
+  { skill: "Skill 2", stat: "Stat 2" },
+  // Add more here
+];
+
+const resources = [
+  // Add resources here
+];
+
+const tiers = [
+  { levelRange: [1, 4], dc: 14, tier: 1 },
+  { levelRange: [5, 8], dc: 16, tier: 2 },
+  { levelRange: [9, 12], dc: 18, tier: 3 },
+  { levelRange: [13, 16], dc: 20, tier: 4 },
+  { levelRange: [17, 20], dc: 21, tier: 5 },
+];
+
+function EventsApp() {
+  const [level, setLevel] = useState(1);
+  const [terrain, setTerrain] = useState("");
+  const [faction, setFaction] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Calculate tier and DC here
+
+    // Generate event here
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Events App</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Level:
+          <input type="number" value={level} onChange={(e) => setLevel(e.target.value)} min="1" max="20" />
+        </label>
+
+        <label>
+          Terrain:
+          <input type="text" value={terrain} onChange={(e) => setTerrain(e.target.value)} />
+        </label>
+
+        <label>
+          Faction:
+          <input type="text" value={faction} onChange={(e) => setFaction(e.target.value)} />
+        </label>
+
+        <button type="submit">Generate Event</button>
+      </form>
+
+      {/* Output goes here */}
     </div>
   );
 }
 
-export default App;
+export default EventsApp;
