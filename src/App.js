af91f55c0c9ba52aa34839409b53b81da1e60e4c
kostchei <56498970+kostchei@users.noreply.github.com>
2023-05-16 13:13:18 +0800

    structure of sentences

--- src/App.js
+++ src/App.js
@@ -102,20 +102,20 @@ const handleSubmit = (event) => {
   if (randomCheckType === "Simple Skill Check") {
     output = (
       <div style={{fontFamily: 'Helvetica Neue, Arial, sans-serif', margin: '0 auto', width: '75%', textAlign: 'left'}}>
-       You meet {name}, {pronoun} is a member of the {selectedFaction}. <br /> You cut a deal with them <strong>Simple Skill Check</strong> DC {dc} {randomSkill.skill} ({randomSkill.stat}). On success you gain {randomGainResource.name} ({gainResourceDescription}). On failure costs you {randomLossResource.name} ({lossResourceDescription}).
+       You meet {name}, {pronoun} is a member of the {selectedFaction}. <br /> Make a <strong>Simple Skill Check</strong> DC {dc} {randomSkill.skill} ({randomSkill.stat}). Success provides {randomGainResource.name} {gainResourceDescription}. On failure you suffer {randomLossResource.name} {lossResourceDescription}.
       </div>
     );
   } else if (randomCheckType === "Resource Swap") {
     output = (
       <div style={{fontFamily: 'Helvetica Neue, Arial, sans-serif', margin: '0 auto', width: '75%', textAlign: 'left'}}>
-        You meet {name}, {pronoun} is a member of the {selectedFaction}. <br /> You strike a  bargain for a <strong>Resource Swap</strong>, on a Skill check DC {dc - 3} {randomSkill.skill} ({randomSkill.stat}). On success you gain {randomGainResource.name} ({gainResourceDescription}) and costs {randomLossResource.name} ({lossResourceDescription}). Failure costs an additional {randomLossResource2.name} ({lossResourceDescription2}).
+        You meet {name}, {pronoun} is a member of the {selectedFaction}. <br /> You strike a  bargain for a <strong>Resource Swap</strong>, make a Skill check DC {dc - 3} {randomSkill.skill} ({randomSkill.stat}). Success provides {randomGainResource.name} {gainResourceDescription} and costs {randomLossResource.name} {lossResourceDescription}. On failure you suffer an additional {randomLossResource2.name} ({lossResourceDescription2}).
       </div>
     );
   } else { // Skill Challenge
     output = (
       <div style={{fontFamily: 'Helvetica Neue, Arial, sans-serif', margin: '0 auto', width: '75%', textAlign: 'left'}}>
        You meet {name}, {pronoun} is a member of the {selectedFaction}. <br /> You face a <strong>Skill Challenge</strong> DC {dc - 3} {randomSkill.skill} ({randomSkill.stat}). <br /> As a group you must achieve twice the number of successes as participants, before you have failures equal to the number of participants. 
-       <br />  On success you gain {randomGainResource.name} ({gainResourceDescription}). Failure costs {randomLossResource.name} ({lossResourceDescription}).
+       <br />  Success provides {randomGainResource.name} {gainResourceDescription}. On failure you suffer {randomLossResource.name} {lossResourceDescription}.
       </div>
     );
   }
